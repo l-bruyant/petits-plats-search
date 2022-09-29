9110cf1d369bc8f73b632228295b587d41bbdcdf
Luc Bruyant <lbruyant@outlook.fr>
2022-09-29 14:45:35 +0200

    Ouverture des filtres

--- scripts/utils/filtersMenus.js
+++ scripts/utils/filtersMenus.js
@@ -1,6 +1,11 @@
+// INGREDIENTS MENU
+
 function displayIngredientsMenu () {
+  hideUstensilesMenu()
+  hideAppareilsMenu()
   document.getElementById('ingredients-expanded').style.display = 'flex'
   document.getElementById('ingredients-filter').style.display = 'none'
+  document.getElementById('text-input-ingredients').focus()
 }
 
 document.getElementById('ingredients-filter').addEventListener('click', displayIngredientsMenu)
@@ -11,3 +16,41 @@ function hideIngredientsMenu () {
 }
 
 document.getElementById('ingredients-filter-icon-up').addEventListener('click', hideIngredientsMenu)
+
+// APPAREILS MENU
+
+function displayAppareilsMenu () {
+  hideUstensilesMenu()
+  hideIngredientsMenu()
+  document.getElementById('appareils-expanded').style.display = 'flex'
+  document.getElementById('appareils-filter').style.display = 'none'
+  document.getElementById('text-input-appareils').focus()
+}
+
+document.getElementById('appareils-filter').addEventListener('click', displayAppareilsMenu)
+
+function hideAppareilsMenu () {
+  document.getElementById('appareils-expanded').style.display = 'none'
+  document.getElementById('appareils-filter').style.display = 'flex'
+}
+
+document.getElementById('appareils-filter-icon-up').addEventListener('click', hideAppareilsMenu)
+
+// USTENSILES MENU
+
+function displayUstensilesMenu () {
+  hideIngredientsMenu()
+  hideAppareilsMenu()
+  document.getElementById('ustensiles-expanded').style.display = 'flex'
+  document.getElementById('ustensiles-filter').style.display = 'none'
+  document.getElementById('text-input-ustensiles').focus()
+}
+
+document.getElementById('ustensiles-filter').addEventListener('click', displayUstensilesMenu)
+
+function hideUstensilesMenu () {
+  document.getElementById('ustensiles-expanded').style.display = 'none'
+  document.getElementById('ustensiles-filter').style.display = 'flex'
+}
+
+document.getElementById('ustensiles-filter-icon-up').addEventListener('click', hideUstensilesMenu)
