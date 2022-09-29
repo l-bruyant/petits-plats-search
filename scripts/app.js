d56f5441d1443f1bf8a0775fca78c2bc836b90ef
Luc Bruyant <lbruyant@outlook.fr>
2022-09-29 19:09:33 +0200

    Ajout des bons items dans tous les filtres

--- scripts/app.js
+++ scripts/app.js
@@ -10,34 +10,56 @@ class App {
     const searchValue = document.getElementById('search-bar').value
     document.getElementById('ingredients-items-list').innerHTML = ''
     let ingredientsListFilter = []
+    let applianceListFilter = []
+    let ustensilsListFilter = []
+
     emptyRecipesDOM()
+    resetIngredientsFilterDOM()
+    resetAppareilsFilterDOM()
+    resetUstensilesFilterDOM()
     recipesData
     // eslint-disable-next-line no-undef
       .map(recipe => new Recipe(recipe))
       .forEach(recipe => {
         const ingredientsNamesString = recipe.ingredients.map(function (a) { return a.ingredient }).toString()
         const ingredientsNames = recipe.ingredients.map(function (a) { return a.ingredient })
+        const applianceName = recipe.appliance
+        const ustensilsNames = recipe.ustensils
         const searchValueCondition = recipe.name.toLowerCase().includes(searchValue.toLowerCase()) || recipe.description.toLowerCase().includes(searchValue.toLowerCase()) || ingredientsNamesString.toLowerCase().includes(searchValue.toLowerCase())
         const searchLengthCondition = searchValue.length > 2
         if (searchValueCondition && searchLengthCondition) {
-          unemptyRecipesDom()
           // eslint-disable-next-line no-undef
           const Template = new RecipeCard(recipe)
           this.recipesWrapper.appendChild(Template.createRecipeCard())
           ingredientsListFilter = ingredientsListFilter.concat(ingredientsNames)
+          applianceListFilter = applianceListFilter.concat(applianceName)
+          ustensilsListFilter = ustensilsListFilter.concat(ustensilsNames)
         } else if (searchValueCondition) {
           ingredientsListFilter = ingredientsListFilter.concat(ingredientsNames)
+          applianceListFilter = applianceListFilter.concat(applianceName)
+          ustensilsListFilter = ustensilsListFilter.concat(ustensilsNames)
         }
       })
     const uniqueIngredients = [...new Set(ingredientsListFilter)]
-    // else {
-    //   document.getElementById('ingredients-expanded').style.gridTemplateRows = 'repeat(6, auto)'
-    // }
     uniqueIngredients.forEach(uniqueIngredient => {
       const ingredientHTML = document.createElement('div')
       ingredientHTML.classList.add('filter-item')
       ingredientHTML.innerText = uniqueIngredient
       document.getElementById('ingredients-items-list').appendChild(ingredientHTML)
+    })
+    const uniqueAppliances = [...new Set(applianceListFilter)]
+    uniqueAppliances.forEach(uniqueAppliance => {
+      const applianceHTML = document.createElement('div')
+      applianceHTML.classList.add('filter-item')
+      applianceHTML.innerText = uniqueAppliance
+      document.getElementById('appareils-items-list').appendChild(applianceHTML)
+    })
+    const uniqueUstenslils = [...new Set(ustensilsListFilter)]
+    uniqueUstenslils.forEach(uniqueUstensil => {
+      const ustensilHTML = document.createElement('div')
+      ustensilHTML.classList.add('filter-item')
+      ustensilHTML.innerText = uniqueUstensil
+      document.getElementById('ustensiles-items-list').appendChild(ustensilHTML)
     })
   }
 
@@ -57,18 +79,18 @@ function emptyRecipesDOM () {
   document.getElementById('results').innerHTML = '<div>Veuillez préciser votre recherche</div>'
 }
 
-function unemptyRecipesDom () {
-  document.getElementById('results').innerHTML = ''
+// function unemptyRecipesDom () {
+//   document.getElementById('results').innerHTML = ''
+// }
+
+function resetIngredientsFilterDOM () {
+  document.getElementById('ingredients-items-list').innerHTML = ''
 }
 
-// function resetIngredientsFilterDOM () {
-//   document.getElementById('ingredients-items-list').innerHTML = ''
-// }
+function resetAppareilsFilterDOM () {
+  document.getElementById('appareils-items-list').innerHTML = ''
+}
 
-// function resetAppareilsFilterDOM () {
-//   document.getElementById('appareils-items-list').innerHTML = ''
-// }
-
-// function resetUstensilesFilterDOM () {
-//   document.getElementById('ustensiles-items-list').innerHTML = ''
-// }
+function resetUstensilesFilterDOM () {
+  document.getElementById('ustensiles-items-list').innerHTML = ''
+}
